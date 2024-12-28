0695a7a6c0495f39f21ef4ce7dc14874d0c9fb90
SUMANTH567374 <sumanth8383@gmail.com>
2024-12-28 16:15:37 +0530

    Wine_Quality

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from Wine_Quality.pipeline.prediction import PredictionPipeline
 def main():
     st.title("Wine Quality Prediction")
 
-    # Using integer values for inputs
+
     fixed_acidity = st.number_input("Fixed Acidity", min_value=4, max_value=15, value=7, step=1, format="%d")
     volatile_acidity = st.number_input("Volatile Acidity", min_value=1, max_value=15, value=3, step=1, format="%d")
     citric_acid = st.number_input("Citric Acid", min_value=0, max_value=10, value=3, step=1, format="%d")
@@ -21,18 +21,15 @@ def main():
     alcohol = st.number_input("Alcohol", min_value=8, max_value=15, value=10, step=1, format="%d")
 
     if st.button("Predict Wine Quality"):
-        # Collecting inputs into an array
+
         data = np.array([fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides,
                          free_sulfur_dioxide, total_sulfur_dioxide, density, pH, sulphates, alcohol]).reshape(1, -1)
         
-        # Using the PredictionPipeline class for prediction
         obj = PredictionPipeline()
         predict = obj.predict(data)
 
-        # Round the predicted value to the nearest integer
         predicted_quality = round(predict[0])
 
-        # Ensure the prediction is within the expected wine quality range (1 to 10)
         predicted_quality = max(1, min(predicted_quality, 10))
 
         st.write(f"Predicted Wine Quality: {predicted_quality}")
